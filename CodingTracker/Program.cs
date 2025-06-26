fd195d934262498a27526ad1419bbe0e1a3e428c
ammujack07 <jarshiniammu@gmail.com>
2025-06-26 14:11:07 +0530

    inital Commit

--- CodingTracker/Program.cs
+++ CodingTracker/Program.cs
@@ -1,7 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, C# Acadamy!  One more Modification");
-Console.WriteLine("Hello, C# Acadamy!  One more Modification");
-Console.WriteLine("Hello, C# Acadamy!  One more Modification");
-Console.WriteLine("Hello, C# Acadamy!  One more Modification");
-Console.WriteLine("Hello, C# Acadamy!  One more Modification");
-Console.WriteLine("Hello, C# Acadamy!  One more Modification");
+
+void MyFirstMethod()
+{
+
+}
+
+void MySecondMethod()
+{
+
+}
+
+void MyThirdMethod()
+{
+
+}
